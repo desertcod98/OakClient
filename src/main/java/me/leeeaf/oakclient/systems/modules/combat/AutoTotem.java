525b8c42d8bb819af9d1b92c5e9c0ad4c62bf018
des98 <desertcod98@gmail.com>
2022-12-04 17:10:20 +0100

    fixed stuff ;)

---
+++ src/main/java/me/leeeaf/oakclient/systems/modules/combat/AutoTotem.java
@@ -0,0 +1,41 @@
+package me.leeeaf.oakclient.systems.modules.combat;
+
+import me.leeeaf.oakclient.event.EventBus;
+import me.leeeaf.oakclient.event.IEventListener;
+import me.leeeaf.oakclient.event.events.TotemUsedEvent;
+import me.leeeaf.oakclient.gui.setting.IntegerSetting;
+import me.leeeaf.oakclient.systems.modules.Category;
+import me.leeeaf.oakclient.systems.modules.Module;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.slot.SlotActionType;
+
+import static me.leeeaf.oakclient.OakClientClient.mc;
+
+public class AutoTotem extends Module {
+//    private final IntegerSetting delay = new IntegerSetting("Delay", "delay","Time in ticks to wait before putting totem in offhand", ()->true, 0, 2000, 50); todo HOW
+    //todo works only in local (maybe dont use event but onTick)
+
+    public AutoTotem() {
+        super("AutoTotem", "Place totems in your offhand", ()->true, true, Category.COMBAT);
+    }
+
+
+    @Override
+    public void onTick() {
+        if(!mc.player.getInventory().offHand.get(0).getItem().equals(Items.TOTEM_OF_UNDYING)){
+            boolean itemInOffhand = !mc.player.getOffHandStack().isEmpty();
+            for(int i = 0; i < PlayerInventory.MAIN_SIZE; i++){
+                ItemStack itemStack = mc.player.getInventory().getStack(i);
+                if(itemStack.getItem().equals(Items.TOTEM_OF_UNDYING)){
+                    mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, i, 0, SlotActionType.PICKUP, mc.player);
+                    mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, 45, 0, SlotActionType.PICKUP, mc.player); //offhand index == 45 :( why the fuck
+                    if(itemInOffhand){
+                        mc.interactionManager.clickSlot(mc.player.currentScreenHandler.syncId, i, 0, SlotActionType.PICKUP, mc.player);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/me/leeeaf/oakclient/systems/modules/combat/AutoWeb.java
+++ src/main/java/me/leeeaf/oakclient/systems/modules/combat/AutoWeb.java
@@ -1,6 +1,7 @@
 package me.leeeaf.oakclient.systems.modules.combat;
 
 import me.leeeaf.oakclient.gui.setting.BooleanSetting;
+import me.leeeaf.oakclient.gui.setting.DoubleSetting;
 import me.leeeaf.oakclient.systems.modules.Category;
 import me.leeeaf.oakclient.systems.modules.Module;
 import me.leeeaf.oakclient.utils.EntityUtils;
@@ -15,15 +16,18 @@ import static me.leeeaf.oakclient.OakClientClient.mc;
 public class AutoWeb extends Module {
     private final BooleanSetting swapBack = new BooleanSetting("Swap back", "swapBack", "Swap back to previous item", () -> true, true);
     private final BooleanSetting airPlace = new BooleanSetting("Air place", "airPlace", "Places blocks in air", () -> true, false);
+    private final DoubleSetting range = new DoubleSetting("Range", "range", "Range to search players in",()->true,0,5,4.5);
 
     public AutoWeb() {
         super("AutoWeb", "Places webs on players", () -> true, true, Category.COMBAT);
         settings.add(swapBack);
+        settings.add(airPlace);
+        settings.add(range);
     }
 
     @Override
     public void onTick() {
-        for (PlayerEntity player : EntityUtils.getPlayers(5, EntityUtils.SortMethod.DISTANCE)) {
+        for (PlayerEntity player : EntityUtils.getPlayers(range.getValue(), EntityUtils.SortMethod.DISTANCE)) {
             int webSlot;
             if ((webSlot = InventoryUtils.getItemSlotInHotbar(Items.COBWEB)) != -1) {
                 if (BlockUtils.canPlace(player.getBlockPos(), false)) {
