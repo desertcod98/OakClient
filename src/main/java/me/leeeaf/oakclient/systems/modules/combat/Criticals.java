2e9eebcb9e327005365078e7d4e79dfe42067c14
des98 <desertcod98@gmail.com>
2023-01-05 15:27:03 +0100

    Changed criticals structure and now sometimes it bypasses (ugly code)

--- src/main/java/me/leeeaf/oakclient/systems/modules/combat/Criticals.java
+++ src/main/java/me/leeeaf/oakclient/systems/modules/combat/Criticals.java
@@ -20,40 +20,62 @@ public class Criticals extends Module {
         //code that needs to be triggered (class PlayerEntity):
         //boolean bl3 = bl && this.fallDistance > 0.0F && !this.onGround && !this.isClimbing() && !this.isTouchingWater() && !this.hasStatusEffect(StatusEffects.BLINDNESS) && !this.hasVehicle() && target instanceof LivingEntity;
     }
+    private int critPacketsToSend;
+    private LivingEntity entityToAttack;
+    private boolean wasSprinting;
+    private double[] yOffsets = {0.08, 0.12, 0.02}; //TODO this values sometimes bypass NCP and sometimes trigger fallheight mismatch (and seem to trigger survival fly a lot)
+    boolean attacked;
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        critPacketsToSend = 0;
+    }
+
     @EventSubscribe
     public void onPacketSend(PacketEvent.Send event) {
         if (event.packet instanceof PlayerInteractEntityC2SPacket packet) {
             if (PlayerInteractEntityC2SUtils.getInteractType(packet) == PlayerInteractEntityC2SUtils.InteractType.ATTACK
-                    && PlayerInteractEntityC2SUtils.getEntity(packet) instanceof LivingEntity) {
-                sendCritPackets();
+                    && PlayerInteractEntityC2SUtils.getEntity(packet) instanceof LivingEntity entity && !attacked) {
+                if (!(mc.player.isClimbing() || mc.player.isTouchingWater()
+                        || mc.player.hasStatusEffect(StatusEffects.BLINDNESS) || mc.player.hasVehicle())) {
+                    entityToAttack = entity;
+                    critPacketsToSend = 3;
+                    wasSprinting = mc.player.isSprinting();
+                    event.cancel();
+                }
+            } else if (attacked) {
+                attacked = false;
             }
         }
     }
 
-    private void sendCritPackets() {
-        if (mc.player.isClimbing() || mc.player.isTouchingWater()
-                || mc.player.hasStatusEffect(StatusEffects.BLINDNESS) || mc.player.hasVehicle()) {
-            return;
-        }
+    @Override
+    public void onTick() {
+        //TODO sending too many packets in one tick doesn't work, but sending one each tick is very slow
+        super.onTick();
+        if (critPacketsToSend > 0) {
+            if (wasSprinting && mc.player.isSprinting()) {
+                mc.player.setSprinting(false);
+                mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.STOP_SPRINTING));
+            }
 
-        boolean sprinting = mc.player.isSprinting();
-        if (sprinting) {
-            mc.player.setSprinting(false);
-            mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.STOP_SPRINTING));
-        }
+            if (mc.player.isOnGround()) {
+                double x = mc.player.getX();
+                double y = mc.player.getBoundingBox().minY;
+                double z = mc.player.getZ();
+                mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + yOffsets[yOffsets.length - critPacketsToSend], z, false));
 
-        if (mc.player.isOnGround()) {
-            double x = mc.player.getX();
-            double y = mc.player.getY();
-            double z = mc.player.getZ();
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.080, z, false));
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.12, z, false)); //TODO sometimes bypasses ncp, sometimes gets fallheightmismatch
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(x, y + 0.076, z, false));
-
-        }
-        if (sprinting) {
-            mc.player.setSprinting(true);
-            mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.START_SPRINTING));
+            }
+            critPacketsToSend--;
+            if (critPacketsToSend == 0) {
+                if (wasSprinting) {
+                    mc.player.setSprinting(true);
+                    mc.player.networkHandler.sendPacket(new ClientCommandC2SPacket(mc.player, ClientCommandC2SPacket.Mode.START_SPRINTING));
+                }
+                mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(entityToAttack, false));
+                attacked = true;
+            }
         }
     }
 
