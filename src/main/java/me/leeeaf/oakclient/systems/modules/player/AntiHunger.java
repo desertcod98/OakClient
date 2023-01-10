acb43cf797976bf8a278da19ffa0fe0fb2300fb7
des98 <desertcod98@gmail.com>
2023-01-10 17:16:59 +0100

    Added pauseInLiquids for AntiHunger

--- src/main/java/me/leeeaf/oakclient/systems/modules/player/AntiHunger.java
+++ src/main/java/me/leeeaf/oakclient/systems/modules/player/AntiHunger.java
@@ -2,19 +2,27 @@ package me.leeeaf.oakclient.systems.modules.player;
 
 import me.leeeaf.oakclient.event.EventSubscribe;
 import me.leeeaf.oakclient.event.events.PacketEvent;
+import me.leeeaf.oakclient.gui.setting.BooleanSetting;
 import me.leeeaf.oakclient.systems.modules.Category;
 import me.leeeaf.oakclient.systems.modules.Module;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 
+import static me.leeeaf.oakclient.OakClientClient.mc;
+
 public class AntiHunger extends Module {
+    private final BooleanSetting pauseInLiquids = new BooleanSetting("Pause in liquids", "pauseInLiquids", "Pause module in liquids", ()->true, true);
+
     public AntiHunger() {
-        //TODO if you are in liquids in servers it creates problems
         super("AntiHunger", "REDUCES hunger consumption", ()->true, true, Category.PLAYER);
+        settings.add(pauseInLiquids);
     }
 
     @EventSubscribe
     public void onPacketSend(PacketEvent.Send event) {
-        if (event.packet instanceof ClientCommandC2SPacket ) {
+        if (event.packet instanceof ClientCommandC2SPacket) {
+            if(pauseInLiquids.getValue() && (mc.player.isTouchingWater() || mc.player.isInLava())){
+                return;
+            }
             ClientCommandC2SPacket.Mode mode = ((ClientCommandC2SPacket) (event).packet).getMode();
 
             if (mode == ClientCommandC2SPacket.Mode.START_SPRINTING || mode == ClientCommandC2SPacket.Mode.STOP_SPRINTING) {
